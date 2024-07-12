5625b90b1894c8b6fc8b40738a185624eefd2fe5
MOONL0323 <103804319+MOONL0323@users.noreply.github.com>
2024-07-12 17:38:34 +0800

    Update User.java
    change author to AAAAAAQQQQQ

--- src/main/java/com/ahua/myRPC_00/common/User.java
+++ src/main/java/com/ahua/myRPC_00/common/User.java
@@ -3,7 +3,7 @@ package com.ahua.myRPC_00.common;
 import java.io.Serializable;
 
 /**
- * @author 86188
+ * @author AAAAAAQQQQQ
  */
 public class User implements Serializable {
     private Integer id;
